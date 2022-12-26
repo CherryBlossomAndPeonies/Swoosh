5cc4dff472d93fc0e2163d0b4126f6c7f13387a6
Bhargavi Nagaraj Naik <bhargavinagarajnaik@clarivate.com>
2022-12-26 17:18:48 +0530

    Lesson 03

--- app/src/main/java/com/bali25/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/bali25/swoosh/WelcomeActivity.kt
@@ -1,11 +1,20 @@
 package com.bali25.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bali25.swoosh.databinding.ActivityWelcomeBinding
+import androidx.databinding.DataBindingUtil
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
+        binding.getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
